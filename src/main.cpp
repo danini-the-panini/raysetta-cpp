b105738ba8d117592a351469e8e683fcc9f1140f
Dani Smith <code@danini.dev>
2026-04-06 23:39:38 +0200

    lambert

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,15 @@
 #include "sphere.hpp"
 #include "camera.hpp"
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, int depth, const hittable& world) {
+  // If we've exceeded the ray bounce limit, no more light is gathered.
+  if (depth <= 0)
+    return color(0,0,0);
+
   hit_record rec;
-  if (world.hit(r, interval(0, infinity), rec)) {
-    return 0.5 * (rec.normal + color(1,1,1));
+  if (world.hit(r, interval(0.001, infinity), rec)) {
+    vec3 direction = rec.normal + random_unit_vector();
+    return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
   }
 
   vec3 unit_direction = unit_vector(r.direction());
@@ -75,7 +80,7 @@ int main(int argc, char** argv) {
       color pixel_color(0,0,0);
       for (unsigned int sample = 0; sample < samples; sample++) {
         ray r = cam.get_ray(i, j);
-        pixel_color += ray_color(r, world);
+        pixel_color += ray_color(r, depth, world);
       }
       write_color(std::cout, pixel_samples_scale * pixel_color);
     }
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -4,6 +4,8 @@
 #include <cmath>
 #include <iostream>
 
+#include "util.hpp"
+
 class vec3 {
   public:
     double e[3];
@@ -20,29 +22,37 @@ class vec3 {
     double& operator[](int i) { return e[i]; }
 
     vec3& operator+=(const vec3& v) {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        e[2] += v.e[2];
-        return *this;
+      e[0] += v.e[0];
+      e[1] += v.e[1];
+      e[2] += v.e[2];
+      return *this;
     }
 
     vec3& operator*=(double t) {
-        e[0] *= t;
-        e[1] *= t;
-        e[2] *= t;
-        return *this;
+      e[0] *= t;
+      e[1] *= t;
+      e[2] *= t;
+      return *this;
     }
 
     vec3& operator/=(double t) {
-        return *this *= 1/t;
+      return *this *= 1/t;
     }
 
     double length() const {
-        return std::sqrt(length_squared());
+      return std::sqrt(length_squared());
     }
 
     double length_squared() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+      return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    static vec3 random() {
+      return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+      return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
     }
 };
 
@@ -53,47 +63,64 @@ using point3 = vec3;
 // Vector Utility Functions
 
 inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
-    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+  return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
 inline vec3 operator+(const vec3& u, const vec3& v) {
-    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+  return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
 inline vec3 operator-(const vec3& u, const vec3& v) {
-    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+  return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 inline vec3 operator*(const vec3& u, const vec3& v) {
-    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+  return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3& v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+  return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator*(const vec3& v, double t) {
-    return t * v;
+  return t * v;
 }
 
 inline vec3 operator/(const vec3& v, double t) {
-    return (1/t) * v;
+  return (1/t) * v;
 }
 
 inline double dot(const vec3& u, const vec3& v) {
-    return u.e[0] * v.e[0]
-         + u.e[1] * v.e[1]
-         + u.e[2] * v.e[2];
+  return u.e[0] * v.e[0]
+    + u.e[1] * v.e[1]
+    + u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3& u, const vec3& v) {
-    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+  return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+    u.e[2] * v.e[0] - u.e[0] * v.e[2],
+    u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(const vec3& v) {
-    return v / v.length();
+  return v / v.length();
+}
+
+inline vec3 random_unit_vector() {
+  while (true) {
+    auto p = vec3::random(-1,1);
+    auto lensq = p.length_squared();
+    if (1e-160 < lensq && lensq <= 1) 
+      return p / sqrt(lensq);
+  }
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+  vec3 on_unit_sphere = random_unit_vector();
+  if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+    return on_unit_sphere;
+  else
+    return -on_unit_sphere;
 }
 
 #endif
